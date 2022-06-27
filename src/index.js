298487ffac823e6969540433f71f133376ee3740
Harshad <harsh.haou@gmail.com>
2022-06-27 13:35:22 +0530

    redux uncomment

--- src/index.js
+++ src/index.js
@@ -1,32 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-// import { Provider } from 'react-redux';
-// import { createStore, applyMiddleware, compose } from 'redux';
-// import thunk from 'redux-thunk';
-// import rootReducer from './store/reducers';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './store/reducers';
 import App from './App';
 import './styles/main.css';
 import './styles/variables.less';
 
-// const initialState = {};
+const initialState = {};
 
-// const store = createStore(
-//   rootReducer,
-//   initialState,
-//   compose(
-//     applyMiddleware(thunk),
-//     (window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ &&
-//       window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()) ||
-//       compose,
-//   ),
-// );
+const store = createStore(
+  rootReducer,
+  initialState,
+  compose(
+    applyMiddleware(thunk),
+    (window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ &&
+      window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()) ||
+      compose,
+  ),
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <Provider store={store}>
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  // </Provider>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+    ,
+  </Provider>,
 );
